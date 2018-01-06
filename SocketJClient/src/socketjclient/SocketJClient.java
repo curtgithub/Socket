c7784db7bba98e9851f8b828b9f94d48cdb50163
owner <owner@Owner01>
2018-01-05 21:39:50 -0500

    Replaced System.out.println with Logger.

--- SocketJClient/src/socketjclient/SocketJClient.java
+++ SocketJClient/src/socketjclient/SocketJClient.java
@@ -15,50 +15,61 @@ import java.util.logging.Logger;
  */
 public class SocketJClient {
 
-    public static void main(String[] args) {
-        String serverName = "localhost";
-        int port = 8080;
-        Socket server = null;
-        System.out.println("Connecting to " + serverName + " on port " + port);
+   public static void main(String[] args) {
 
-        try {
-            server = new Socket(serverName, port);
-        } catch (IOException ex) {
-            System.out.println("CLIENT server is not running!");
-            System.exit(-1);
-        }
+      Logger logger = Logger.getLogger("SocketJClient");
 
-        int nbytes = 1024;
-        byte[] ibytes = new byte[nbytes];
-       
-        try {
+      String serverName = "localhost";
+      int port = 8080;
+      Socket server = null;
 
-            System.out.println("CLIENT Connected to server: " + server.getRemoteSocketAddress());
-            System.out.println("CLIENT server = " + server.toString());
+      String msg = "Connecting to " + serverName + " on port " + port;
+      logger.log(Level.INFO, msg);
 
-            InputStream inFromServer = server.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
-            OutputStream outToServer = server.getOutputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
-            
-            for (int i = 0; i < 10; i++) {
-                
-                String omsg = "Requst #" + (i + 1);
-                byte[] obytes = omsg.getBytes();
-                out.write(obytes, 0, obytes.length);
-                System.out.println("CLIENT sent to server -> " + omsg);
-                
-                nbytes = in.read(ibytes, 0, ibytes.length);
-                System.out.println("CLIENT: read nbytes = " + nbytes);
-                String imsg = new String(ibytes, 0, nbytes);
-                imsg = imsg + "\n";
-                System.out.println("CLIENT: received message -> " + imsg);
-                
-            }
+      try {
+         server = new Socket(serverName, port);
+      } catch (IOException ex) {
+         msg = "Server is not running!";
+         logger.log(Level.SEVERE, msg);
+         System.exit(-1);
+      }
 
-            server.close();
-        } catch (IOException ex) {
-            Logger.getLogger(SocketJClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+      int nbytes = 1024;
+      byte[] ibytes = new byte[nbytes];
+
+      try {
+
+         msg = "Connected to server: " + server.getRemoteSocketAddress();
+         logger.log(Level.INFO, msg);
+         msg = "Server = " + server.toString();
+         logger.log(Level.INFO, msg);
+
+         InputStream inFromServer = server.getInputStream();
+         DataInputStream in = new DataInputStream(inFromServer);
+         OutputStream outToServer = server.getOutputStream();
+         DataOutputStream out = new DataOutputStream(outToServer);
+
+         for (int i = 0; i < 10; i++) {
+
+            String omsg = "Requst #" + (i + 1);
+            byte[] obytes = omsg.getBytes();
+            out.write(obytes, 0, obytes.length);
+            msg = "Sent to server -> " + omsg;
+            logger.log(Level.INFO, msg);
+
+            nbytes = in.read(ibytes, 0, ibytes.length);
+            msg = "Read nbytes = " + nbytes;
+            logger.log(Level.INFO, msg);
+            String imsg = new String(ibytes, 0, nbytes);
+            imsg = imsg + "\n";
+            msg = "Received message -> " + imsg;
+            logger.log(Level.INFO, msg);
+
+         }
+
+         server.close();
+      } catch (IOException ex) {
+         Logger.getLogger(SocketJClient.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
 }
